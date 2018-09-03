875c23119b1c35e78f5f63d833804addd2ee8bb4
cktfp656 <cktfp656@gmail.com>
2018-09-03 15:52:16 +0900

    各ページに飛べるようにしました。

--- new_contents.php
+++ new_contents.php
@@ -28,7 +28,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html"><img src="img/logo.png"></a>
+                <a class="navbar-brand" href="index.php"><img src="img/logo.png"></a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
                 <ul class="nav navbar-nav navbar-right">

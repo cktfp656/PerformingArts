1d203bbef4aea9399be0bb70a811b2a3fe93349e
cktfp656 <cktfp656@gmail.com>
2018-09-12 12:58:59 +0900

    各コメントの表示機能と入力機能を表示

--- kimono.php
+++ kimono.php
@@ -1,11 +1,18 @@
 <?php
-	session_start();
-	require('db_connect.php');
-	if(!empty($_POST)){
-		$sql = 'INSERT INTO `comments`(`user_id`,`category_code`,`comment`,`created`) VALUES(?,?,?,now())';
-		$data = array($_SESSION['id'],'kimono',$_POST['comment']);
+    session_start();
+    require('db_connect.php');
+    echo '<br>';
+if(!empty($_POST)){
+    // var_dump($_POST);exit;
+    if(!empty($_POST)){
+        $sql = 'INSERT INTO `comments`(`user_id`,`content_id`,`category_code`,`comment`,`created`) VALUES(?,?,?,?,now())';
+        $data = array($_SESSION['id'],$_POST['content_id'],'kimono',$_POST['comment']);
             $stmt = $dbh->prepare($sql);
             $stmt ->execute($data);
+
+
+
+}
 }
 //着物のコンテンツを取得するSQL文作成
 $sql='SELECT * FROM `contents` WHERE `category_code` = "kimono"';
@@ -19,46 +26,48 @@ while(true){
         if($kimono == false){
             break;
         }
-        $kimono_contents[] = $kimono;
-    }
-     $sql='SELECT * FROM `contents` WHERE `category_code` = "kimono" ORDER BY   `created` DESC LIMIT 0,3';
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
-    $kabuki_contents =array();
-    while(true){
-      $kimono = $stmt->fetch(PDO::FETCH_ASSOC);
-      if($kimono== false){
-        break;
-      }
-       $kimono_contents[] = $kimono;
+
+        $comments_sql = 'SELECT `comments`.`comment`,`users`.`name`,`users`.`img_name` FROM `comments` LEFT JOIN `users` ON `comments`.`user_id`=`users`.`id` WHERE `content_id`=?';
+        $data = array($kimono['id']);
+        $comments_stmt = $dbh->prepare($comments_sql);
+        $comments_stmt->execute($data);
+        $comments = array();
+        while(true){
+            $comment = $comments_stmt->fetch(PDO::FETCH_ASSOC);
+            if($comment == false){
+                break;
+            }
+            $comments[] = $comment;
+        }
+        $kimono['comment'] = $comments;
+        $kimono_contents[] =$kimono;
 
     }
     // echo'<pre>';
-    // var_dump($kimono_contents);
+    // var_dump($kabuki_contents);
     // echo'</pre>';
     // exit;
  ?>
  <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<!DOCTYPE html>
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+    <meta charset="UTF-8">
+    <!DOCTYPE html>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
 
-	<!-- Bootstrap -->
-	<link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
-	<link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
-	<link href="lib/bootstrap/css/form.css" rel="stylesheet">
-	<link href="lib/bootstrap/css/timeline.css" rel="stylesheet">
-	<link href="lib/bootstrap/css/main.css" rel="stylesheet">
-	<!-- designフォルダ内では2つパスの位置を戻ってからcssにアクセスしていることに注意!!! -->
+    <!-- Bootstrap -->
+    <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="lib/bootstrap/css/form.css" rel="stylesheet">
+    <link href="lib/bootstrap/css/timeline.css" rel="stylesheet">
+    <link href="lib/bootstrap/css/main.css" rel="stylesheet">
+    <!-- designフォルダ内では2つパスの位置を戻ってからcssにアクセスしていることに注意!!! -->
 </head>
-<body>
-	<nav class="navbar navbar-default navbar-fixed-top">
+<body style="background-image: url('img/kabuki_back.png');background-position: center center;background-repeat: no-repeat; background-attachment: fixed;background-size: cover;">
+    <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
@@ -73,24 +82,27 @@ while(true){
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><button class="btn btn-info"><a href="index.html" style="color: white;">contact</a></button></li>
-                    <li><button class="btn btn-success"><a href="index.html" style="color: white;">login</a></button></li>
-                    <li><button class="btn btn-danger"><a href="index.html" style="color: white;">logout</a></button></li>
+                    <li><button class="btn btn-info"><a href="kimono.php" style="color: white;">kimono</a></button></li>
+                    <li><button class="btn btn-info"><a href="kabuki.php" style="color: white;">kabuki</a></button></li>
+                    <li><button class="btn btn-info"><a href="rakugo.php" style="color: white;">rakugo</a></button></li>
+                    <li><button class="btn btn-primary"><a href="index.php#portfolid" style="color: white;">photos</a></button></li>
+                    <li><button class="btn btn-warning"><a href="index.php#clients" style="color: white;">comments</a></button></li>
+                    <li><button class="btn btn-success"><a href="login.php" style="color: white;">login</a></button></li>
+                    <li><button class="btn btn-danger"><a href="logout.php" style="color: white;">logout</a></button></li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container-fluid -->
     </nav>
-	<hr>
-	<div class="container" style="padding-top: 70px;">
-		<div class="row" style="border:2px solid;">
-			<?php foreach ($kimono_contents as $kimono): ?>
-			<div class="col-md-6">
-				<?php include("kimono_card.php") ?>
-			</div>
-			<?php endforeach ?>
-	   </div>
+    <div class="container" style="padding-top: 70px;">
+        <div class="row" style="border:2px solid; background: white;">
+            <?php foreach ($kimono_contents as $kimono): ?>
+            <div class="col-md-6">
+                <?php include("kimono_card.php") ?>
+            </div>
+                 <?php endforeach ?>
+       </div>
     </div>
 </body>
 </html>
--- rakugo.php
+++ rakugo.php
@@ -1,13 +1,17 @@
 <?php
-	session_start();
-	require('db_connect.php');
-	if(!empty($_POST)){
-		$sql = 'INSERT INTO `comments`(`user_id`,`category_code`,`comment`,`created`) VALUES(?,?,?,now())';
-		$data = array($_SESSION['id'],'rakugo',$_POST['comment']);
+    session_start();
+    require('db_connect.php');
+    echo '<br>';
+if(!empty($_POST)){
+    // var_dump($_POST);exit;
+    if(!empty($_POST)){
+        $sql = 'INSERT INTO `comments`(`user_id`,`content_id`,`category_code`,`comment`,`created`) VALUES(?,?,?,?,now())';
+        $data = array($_SESSION['id'],$_POST['content_id'],'rakugo',$_POST['comment']);
             $stmt = $dbh->prepare($sql);
             $stmt ->execute($data);
 }
-//落語のコンテンツを取得するSQL文作成
+}
+//着物のコンテンツを取得するSQL文作成
 $sql='SELECT * FROM `contents` WHERE `category_code` = "rakugo"';
 //SQL実行
 $stmt = $dbh->prepare($sql);
@@ -19,24 +23,48 @@ while(true){
         if($rakugo == false){
             break;
         }
-        $rakugo_contents[] = $rakugo;
+
+        $comments_sql = 'SELECT `comments`.`comment`,`users`.`name`,`users`.`img_name` FROM `comments` LEFT JOIN `users` ON `comments`.`user_id`=`users`.`id` WHERE `content_id`=?';
+        $data = array($rakugo['id']);
+        $comments_stmt = $dbh->prepare($comments_sql);
+        $comments_stmt->execute($data);
+        $comments = array();
+        while(true){
+            $comment = $comments_stmt->fetch(PDO::FETCH_ASSOC);
+            if($comment == false){
+                break;
+            }
+            $comments[] = $comment;
+        }
+        $rakugo['comment'] = $comments;
+        $rakugo_contents[] =$rakugo;
+
     }
-?>
-<!DOCTYPE html>
+    // echo'<pre>';
+    // var_dump($kabuki_contents);
+    // echo'</pre>';
+    // exit;
+ ?>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title></title>
-	<!-- Bootstrap -->
-	<link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
-	<link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
-	<link href="lib/bootstrap/css/form.css" rel="stylesheet">
-	<link href="lib/bootstrap/css/timeline.css" rel="stylesheet">
-	<link href="lib/bootstrap/css/main.css" rel="stylesheet">
-	<!-- designフォルダ内では2つパスの位置を戻ってからcssにアクセスしていることに注意!!! -->
+    <meta charset="UTF-8">
+    <!DOCTYPE html>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+    <!-- Bootstrap -->
+    <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="lib/bootstrap/css/form.css" rel="stylesheet">
+    <link href="lib/bootstrap/css/timeline.css" rel="stylesheet">
+    <link href="lib/bootstrap/css/main.css" rel="stylesheet">
+    <!-- designフォルダ内では2つパスの位置を戻ってからcssにアクセスしていることに注意!!! -->
 </head>
-<body>
-	<nav class="navbar navbar-default navbar-fixed-top">
+<body style="background-image: url('img/kabuki_back.png');background-position: center center;background-repeat: no-repeat; background-attachment: fixed;background-size: cover;">
+    <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
@@ -51,9 +79,13 @@ while(true){
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><button class="btn btn-info"><a href="index.html" style="color: white;">contact</a></button></li>
-                    <li><button class="btn btn-success"><a href="index.html" style="color: white;">login</a></button></li>
-                    <li><button class="btn btn-danger"><a href="index.html" style="color: white;">logout</a></button></li>
+                    <li><button class="btn btn-info"><a href="kimono.php" style="color: white;">kimono</a></button></li>
+                    <li><button class="btn btn-info"><a href="kabuki.php" style="color: white;">kabuki</a></button></li>
+                    <li><button class="btn btn-info"><a href="rakugo.php" style="color: white;">rakugo</a></button></li>
+                    <li><button class="btn btn-primary"><a href="index.php#portfolid" style="color: white;">photos</a></button></li>
+                    <li><button class="btn btn-warning"><a href="index.php#clients" style="color: white;">comments</a></button></li>
+                    <li><button class="btn btn-success"><a href="login.php" style="color: white;">login</a></button></li>
+                    <li><button class="btn btn-danger"><a href="logout.php" style="color: white;">logout</a></button></li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
@@ -61,18 +93,13 @@ while(true){
         <!-- /.container-fluid -->
     </nav>
     <div class="container" style="padding-top: 70px;">
-    	<div class="row" style="border:2px solid;">
-			<?php foreach ($rakugo_contents as $rakugo): ?>
-				<div class=col-md-6>
-					<?php include("rakugo_card.php") ?>
-				</div>
-			<?php endforeach ?>
-		</div>
-		<br>
-				</div>
-			</form>
-		</div>
-		</div>
-	</div>
-</dody>
+        <div class="row" style="border:2px solid; background: white;">
+            <?php foreach ($rakugo_contents as $rakugo): ?>
+            <div class="col-md-6">
+                <?php include("rakugo_card.php") ?>
+            </div>
+                 <?php endforeach ?>
+       </div>
+    </div>
+</body>
 </html>
